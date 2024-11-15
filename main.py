9eb8ea970d4ee59425b0572027dcc3b8ddb37e9c
Nuckles222 <thothmes2862@gmail.com>
2024-11-15 14:19:38 +1100

    Added in Factors to Dice Type

--- main.py
+++ main.py
@@ -20,8 +20,23 @@ def roll_all_dice(event):
     global dice_type  # use global var named dice_type
     ...  # replace with your own code
     dice_roll = document.getElementById("D_Rol").value 
-    
-
+    dice_roll=int(dice_roll)
+    if dice_type == "D2":
+        dice_type = 2 
+    elif dice_type == "D4":
+        dice_type == 4
+    elif dice_type == "D6":
+        dice_type == 6
+    elif dice_type == "D8":
+        dice_type == 8
+    elif dice_type == "D10":
+        dice_type == 10
+    elif dice_type == "D12":
+        dice_type == 12
+    elif dice_type == "D20":
+        dice_type == 20 
+    elif dice_type == "D100":
+        dice_type == 100
 def clear_history(event):
     # this finds the div tag with id attribute 'roll-history' and clears whatever is inside
     document.querySelector("div#roll-history").innerHTML = ""
